899fc9faad35ce074587f35692f2b91377d5dbde
ahmedashraf20026@gmail.com <ahmedashraf200261@gmail.com>
2024-06-02 19:23:57 +0300

    second commit

--- js/Assignment_9.js
+++ js/Assignment_9.js
@@ -1,73 +1,37 @@
-// var btn = document.getElementById("btn");
-// var quoteOutputone = document.getElementById("quoteOutput");
-// var quoteOutputtwo = document.getElementById("authorOutput");
-
-// var Quotes=[
-
-//     {'author': 'Jim Rohn',
-//      'quote': 'Beware of what you become in pursuit of what you want.'
-//     },
-//     {'author': 'Epictetus',
-//      'quote': 'It\'s not what happens to you, but how you react to it that matters.'
-//     },
-//     {'author': 'Frank Sinatra',
-//      'quote': 'The best revenge is massive success.'
-//     },
-//     {'author': 'Wayne Gretzy',
-//      'quote': 'You miss 100% of the shots you don\'t take.'
-//     },
-//     {'author': 'Nelson Mandela',
-//      'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
-//     },
-//     {'author': 'Elbert Hubbard',
-//      'quote': 'Do not take life too seriously. You will not get out alive.'
-//     },
-
-// ];
-
-// btn.addEventListener('click',function(){
-//     var randomQuote=Quotes[Math.floor(Math.random()*Quotes.length)]
-//     quoteOutputone.innerHTML=randomQuote;
-//     quoteOutputtwo.innerHTML=randomQuote;
-// })
-
-var productContainer = [];
-
-function generalQoutes() {
-  var coversation = {
-    author: "Jim Rohn",
-    quote: "Beware of what you become in pursuit of what you want.",
-    author: "Epictetus",
-    quote:
-      "It's not what happens to you, but how you react to it that matters.",
-  };
-  
-  
-  productContainer.push(coversation);
-  console.log(coversation);
-  displayData();
-}
-
-function displayData() {
-
-     var cartona="";
-     var cartonaTwo="";
-     console.log(productContainer);
-
-  for (var i = 0; i < productContainer.length; i++) 
-  {
-    
-    cartona +=`Jim Rohn`;
-    console.log(cartona);
-
-    cartonaTwo+=` Beware of what you become in pursuit of what you want.`;
-   console.log(cartonaTwo);
-  }
-  document.getElementById("demo").innerHTML=cartona;
-  document.getElementById("demo_2").innerHTML=cartonaTwo;
- 
+var btn=document.querySelector('#btn');
 
 
 
+var quote=[
 
-}
+{
+  'qoute':"It's not what happens to you, but how you react to it that matters.",
+  'auther':'--Epictetus'
+},
+{
+  'qoute':"The best revenge is massive success.",
+  'auther':'--Frank Sinatra'
+},
+{
+  'qoute':"You miss 100% of the shots you don't take.",
+  'auther':'--Wayne Gretzy'
+},
+{
+  'qoute':"Do not take life too seriously. You will not get out alive.",
+  'auther':'--Elbert Hubbard'
+},
+{
+  'qoute':"Resentment is like drinking poison and waiting for your enemies to die.",
+  'auther':'--Nelson Mandela'
+},
+]
+
+
+btn.addEventListener('click',function()
+{
+  var num=Math.floor(Math.random()*quote.length)
+  console.log(num);
+
+  document.getElementById('quote').innerHTML=quote[num].qoute;
+  document.getElementById('auther').innerHTML=quote[num].auther;
+})
